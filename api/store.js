1091b38005dadd754f40f0fee3254759742624c9
Hyeonsu Son <107483302+dogmania@users.noreply.github.com>
2025-08-23 15:25:07 +0000

    feat: dynamic link 구현

---
+++ api/store.js
@@ -0,0 +1,12 @@
+export default function handler(req, res) {
+  const ua = req.headers['user-agent'] || '';
+
+  if (/iPhone|iPad|iPod/i.test(ua)) {
+    res.writeHead(302, { Location: 'https://apps.apple.com/app/id6740790261' });
+  } else if (/Android/i.test(ua)) {
+    res.writeHead(302, { Location: 'https://play.google.com/store/apps/details?id=com.poptato.app' });
+  } else {
+    res.writeHead(302, { Location: 'https://ondot.kr' });
+  }
+  res.end();
+}
